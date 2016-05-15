5b93a59e1e3d011bf908fed33203e5439927a6e2
enoch232 <enoch232@gmail.com>
2016-05-15 21:40:19 +0900

    add some model relations, as well as fix the index page for the models

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,7 @@
 class HomesController < ApplicationController
 	def index
 	  @homeworks = Homework.all.limit(10)
+	  @topics = Topic.all
+	  @announcements = Announcement.all.limit(10)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,9 @@
 Rails.application.routes.draw do
-  get 'lessons/index'
-
-  get 'lessons/new'
-
-  get 'lessons/create'
-
-  get 'lessons/show'
-
-  get 'lessons/destroy'
-
-  get 'topics/index'
-
-  get 'topics/new'
-
-  get 'topics/create'
-
-  get 'topics/show'
-
-  get 'topics/destroy'
-
-  get 'classes/index'
-
-  get 'classes/new'
-
-  get 'classes/create'
-
-  get 'classes/show'
-
-  get 'classes/destroy'
-
-  get 'announcements/index'
-
-  get 'announcements/new'
-
-  get 'announcements/create'
-
-  get 'announcements/destroy'
-
-  get 'announcements/show'
-
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lessons
+  resources :topics
+  resources :announcements
   resources :homeworks
   root to: 'homes#index'
 end
