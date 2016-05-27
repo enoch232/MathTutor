d331a58999c5b713a642e11fe9c68ef665ed0b82
enoch232 <enoch232@gmail.com>
2016-05-27 20:59:31 +0900

    fix production error 1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  config.action_cable.url = 'wss://thinkpowermath.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://thinkpowermath.herokuapp.com', /http:\/\/thinkpowermath.herokuapp*/ ]
+  config.action_cable.url = 'wss://thinkpowermath.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://thinkpowermath.com', /http:\/\/thinkpowermath*/ ]
   # Don't mount Action Cable in the main server process.
   # config.action_cable.mount_path = nil
 
