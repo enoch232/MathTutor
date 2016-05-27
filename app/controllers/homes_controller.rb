23588148eb896ec5109129547791be148de7cd86
enoch232 <enoch232@gmail.com>
2016-05-27 18:57:08 +0900

    add assignment div on index page

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 class HomesController < ApplicationController
 	before_action :authenticate_user!
 	def index
-	  @assignments = Assignment.all.order("assignments.created_at desc").limit(6)
+	  @assignments = Assignment.all.order("assignments.created_at desc").limit(10)
 	  @topics = Topic.all
-	  @announcements = Announcement.all.order("announcements.updated_at desc").limit(6)
+	  @announcements = Announcement.all.order("announcements.updated_at desc").limit(5)
 	end
 end
